4e9f87c7a18ad35ecd5cc68d016ba54e34f29e2e
vishnusahit <81292629+vishnusahit@users.noreply.github.com>
2021-05-20 15:16:58 +0530

    Add files via upload

---
+++ chatbot.js
@@ -0,0 +1,154 @@
+const express = require("express");
+const { WebhookClient } = require("dialogflow-fulfillment");
+const { Payload } =require("dialogflow-fulfillment");
+const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+var randomstring = require("randomstring"); 
+console.log("hello");
+
+app.post("/", express.json(), (req, res) => {
+    const agent = new WebhookClient({ 
+		request: req, response: res 
+		});
+
+
+ async function identify_user(agent)
+{
+  console.log(agent.parameters.phone);
+  console.log("In Heree");
+  const acct_num = agent.parameters.phone;
+  console.log(acct_num);
+  const client = new MongoClient(url, { useUnifiedTopology: true });
+   await client.connect();
+  const snap =  await client.db("internet").collection("users").findOne({number:acct_num});
+  snap.then(data=>snap=data);
+  
+  if(snap==null){
+	   agent.add("Re-Enter your account number");
+
+  }
+  else
+  {
+  const user_name=snap.name;
+  console.log(user_name);
+   agent.add("Welcome  "+user_name+"!!  \n How can I help you");}
+}
+	
+function report_issue(agent)
+{
+ 
+  var issue_vals={1:"Internet Down",2:"Slow Internet",3:"Buffering problem",4:"No connectivity"};
+  
+  const intent_val=agent.parameters.number;
+  console.log
+  
+  var val=issue_vals[intent_val];
+  
+  var trouble_ticket=randomstring.generate(7);
+
+  //Generating trouble ticket and storing it in Mongodb
+  //Using random module
+  MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("chatbot");
+    
+	var u_name = user_name;    
+    var issue_val=  val; 
+    var status="pending";
+
+	let ts = Date.now();
+    let date_ob = new Date(ts);
+    let date = date_ob.getDate();
+    let month = date_ob.getMonth() + 1;
+    let year = date_ob.getFullYear();
+
+    var time_date=year + "-" + month + "-" + date;
+
+	var myobj = { username:u_name, issue:issue_val,status:status,time_date:time_date,trouble_ticket:trouble_ticket };
+
+    dbo.collection("user_issues").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    db.close();    
+  });
+ });
+ agent.add("The issue reported is: "+ val +"\nThe ticket number is: "+trouble_ticket);
+}
+
+//trying to load rich response
+function custom_payload(agent)
+{
+	var payLoadData=
+		{
+  "richContent": [
+    [
+      {
+        "type": "list",
+        "title": "Internet Down",
+        "subtitle": "Press '1' for Internet is down",
+        "event": {
+          "name": "",
+          "languageCode": "",
+          "parameters": {}
+        }
+      },
+      {
+        "type": "divider"
+      },
+      {
+        "type": "list",
+        "title": "Slow Internet",
+        "subtitle": "Press '2' Slow Internet",
+        "event": {
+          "name": "",
+          "languageCode": "",
+          "parameters": {}
+        }
+      },
+	  {
+        "type": "divider"
+      },
+	  {
+        "type": "list",
+        "title": "Buffering problem",
+        "subtitle": "Press '3' for Buffering problem",
+        "event": {
+          "name": "",
+          "languageCode": "",
+          "parameters": {}
+        }
+      },
+      {
+        "type": "divider"
+      },
+      {
+        "type": "list",
+        "title": "No connectivity",
+        "subtitle": "Press '4' for No connectivity",
+        "event": {
+          "name": "",
+          "languageCode": "",
+          "parameters": {}
+        }
+      }
+    ]
+  ]
+}
+agent.add(new Payload(agent.UNSPECIFIED,payLoadData,{sendAsMessage:true, rawPayload:true }));
+}
+
+
+
+
+var intentMap = new Map();
+intentMap.set("Service_intent",identify_user);
+intentMap.set("Service_intent - custom - custom", report_issue);
+intentMap.set("Service_intent - custom", custom_payload);
+
+agent.handleRequest(intentMap);
+
+});//Closing tag of app.post
+
+app.listen(process.env.PORT || 8080);
+
